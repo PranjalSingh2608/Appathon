77cec23037e3611775455713c3ddbf6c2831b204
PranjalSingh2608 <pranjalsingh2608@gmail.com>
2024-03-08 18:09:11 +0530

    added dialog

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:appathon/utils/colors.dart';
 import 'package:appathon/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -26,7 +29,80 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  final TextEditingController milkSoldController = TextEditingController();
+  final TextEditingController soldMilkController = TextEditingController();
+  Future<void> updateStock(int soldMilk) async {
+    final prefs = await SharedPreferences.getInstance();
+    String number = prefs.getString('phoneNo').toString();
+    final apiUrl =
+        'https://smiling-garment-deer.cyclic.app/updatestock/$number';
+
+    try {
+      final response = await http.post(
+        Uri.parse(apiUrl),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'soldMilk': soldMilk}),
+      );
+
+      if (response.statusCode == 200) {
+        final dynamic responseData = json.decode(response.body);
+        _showResponseDialog(responseData);
+      } else {
+        throw Exception(
+            'Failed to update stock. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error: $e');
+      _showErrorDialog();
+    }
+  }
+
+  void _showResponseDialog(Map<String, dynamic> responseData) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Stock Update Successful'),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Total Stock: ${responseData['totalStock']}'),
+              Text('Sold Stock: ${responseData['soldStock']}'),
+              Text('Available Stock: ${responseData['availableStock']}'),
+            ],
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showErrorDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Error'),
+          content: Text('Failed to update stock. Please try again.'),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -96,10 +172,14 @@ class _HomeScreenState extends State<HomeScreen> {
                       padding: const EdgeInsets.only(left: 16.0, right: 16),
                       child: TextField(
                         keyboardType: TextInputType.number,
-                        controller: milkSoldController,
+                        controller: soldMilkController,
                         decoration: InputDecoration(
                           suffixIcon: IconButton(
-                              onPressed: () {},
+                              onPressed: () {
+                                final soldMilk =
+                                    int.tryParse(soldMilkController.text) ?? 0;
+                                updateStock(soldMilk);
+                              },
                               icon: Icon(
                                 Icons.send,
                               )),
